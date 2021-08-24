4d110cd8176f79c4171579192d1d4c0b47d5735d
ROBERT ZIMMERMAN <ROBERT.R.ZIMMERMAN@GMAIL.COM>
2021-08-23 18:36:02 -0700

    testing for on the fly changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,8 @@
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Changes to Title
-        </a>
-      </header>
-    </div>
-  );
-}
+
+
+// API Key = pk.d8a18960b7ff65ca119e042396cdd618
 
 export default App;
