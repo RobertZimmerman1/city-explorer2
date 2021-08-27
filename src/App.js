a098228fc12eda87c6e51669c061276c1b2fd338
ROBERT ZIMMERMAN <ROBERT.R.ZIMMERMAN@GMAIL.COM>
2021-08-26 18:55:21 -0700

    restarting project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import { Component } from 'react';
-import './App.css';
-import Form from './Form';
-import Header from './Header';
-import Footer from './Footer';
-import Table from './Table';
-import Map from './Map';
+// import React from 'react';
+// import { Component } from 'react';
+// import './App.css';
+// import Form from './Form';
+// import Header from './Header';
+// import Footer from './Footer';
+// import Table from './Table';
+// import Map from './Map';
 
-class App extends Component {
-  render() {
-    return (
-      <div className='App'>
-        <Header />
-        <Form />
-        <Table />
-        <Map />
-        <Footer />
-      </div>
-    )
-  }
-}
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className='App'>
+//         <Header />
+//         <Form />
+//         <Table />
+//         <Map />
+//         <Footer />
+//       </div>
+//     )
+//   }
+// }
 
-// API Key = pk.d8a18960b7ff65ca119e042396cdd618
+// // API Key = pk.d8a18960b7ff65ca119e042396cdd618
 
-export default App;
+// export default App;
